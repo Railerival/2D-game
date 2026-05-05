0cef4826f42c56061a56dce80ebfb2b55e9a9d2d
Aarav <gamingtiger2.0.12072010@gmail.com>
2026-05-05 20:10:23 +0530

    Implement background and movement controls in app.py
    Added background image and basic character movement controls.

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 clock =  pygame.time.Clock()
 pygame.display.set_caption("the dungen")#-----------------we can change the name later
+background = pygame.image.load("...")#------------file name
 
 
 while True:
@@ -13,6 +14,19 @@ while True:
         if event.type == pygame.QUIT:#----------when player closes the screen
             pygame.quit()
             exit()
-    #whole game here
+
+        if even.type == pygame.KEYDOWN:
+            if event.key == pygame.k_w or event.key == pygame.K_UP:
+                #move the character forwards
+                pass
+            elif event.key == pygame.k_s or event.key == pygame.K_DOWN:
+                #move the character backwards
+                pass
+            elif event.key == pygame.k_a or event.key == pygame.K_LEFT:
+                #move the character left
+                pass
+            elif event.key == pygame.k_d or event.key == pygame.K_RIGHT:
+                #move the character right
+    screen.blit(background,(0,0))
     pygame.display.update()
     clock.tick(60)#-------------frame rate here its 60 fps

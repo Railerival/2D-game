284dd875ec3a9728ec3ad4e0b6837a15aca9fcf0
aarav <gamingtiger2.0.12072010@gmail.com>
2026-05-07 16:03:19 +0530

    added player movement

--- Game_file/app.py
+++ Game_file/app.py
@@ -3,32 +3,54 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800,400))
-clock =  pygame.time.Clock()
-pygame.display.set_caption("the dungen")#-----------------we can change the name later
-background = pygame.image.load("...")#------------file name
 
+screen = pygame.display.set_mode((800, 400))
+clock = pygame.time.Clock()
+
+pygame.display.set_caption("The Dungeon")#--------------------we can always change the name
+
+# Images
+playerup = pygame.image.load("player-up.png").convert_alpha()
+playerdown = pygame.image.load("player-down.png").convert_alpha()
+playerleft = pygame.image.load("player-left.png").convert_alpha()
+playerright = pygame.image.load("player-right.png").convert_alpha()
+
+# Current player image
+player = playerdown
+
+playerrect = player.get_rect(center=(400, 200))
 
 while True:
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:#----------when player closes the screen
+        if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-        if even.type == pygame.KEYDOWN:
-            if event.key == pygame.k_w or event.key == pygame.K_UP:
-                #move the character forwards
-                pass
-            elif event.key == pygame.k_s or event.key == pygame.K_DOWN:
-                #move the character backwards
-                pass
-            elif event.key == pygame.k_a or event.key == pygame.K_LEFT:
-                #move the character left
-                pass
-            elif event.key == pygame.k_d or event.key == pygame.K_RIGHT:
-                #move the character right
-    screen.blit(background,(0,0))
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_w] or keys[pygame.K_UP]:
+        playerrect.y -= 2
+        player = playerup
+
+    elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        playerrect.y += 2
+        player = playerdown
+
+    elif keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        playerrect.x -= 2
+        player = playerleft
+
+    elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+        playerrect.x += 2
+        player = playerright
+
+    screen.fill((30, 30, 30))
+
+    screen.blit(player, playerrect)
+
     pygame.display.update()
-    clock.tick(60)#-------------frame rate here its 60 fps
+
+    clock.tick(60)
 
 #this might go in __init__.py

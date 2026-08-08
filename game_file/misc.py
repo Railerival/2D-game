23f845bb67b3b4047f4903f7be7fd253e60692f1
Saber_0324 <250929174+Saber0324@users.noreply.github.com>
2026-08-07 23:40:01 -0400

    Add a None check for surface

--- game_file/misc.py
+++ game_file/misc.py
@@ -8,5 +8,7 @@ def display_debug(info, y=10, x=10):
     surface = pygame.display.get_surface()
     text = font.render(str(info), True, "white")
     rect = text.get_rect(topleft=(x, y))
-    pygame.draw.rect(surface, "Black", rect)
-    surface.blit(text, rect)
+
+    if surface is not None:
+        pygame.draw.rect(surface, "Black", rect)
+        surface.blit(text, rect)

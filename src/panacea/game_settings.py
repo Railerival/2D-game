fb8dce2cb747e33d8d3215112d54f534a7afe59d
Saber_0324 <250929174+Saber0324@users.noreply.github.com>
2026-08-10 15:20:11 -0400

    Updated path to assets and added a project root constant

--- src/panacea/game_settings.py
+++ src/panacea/game_settings.py
@@ -2,6 +2,8 @@
 
 from pathlib import Path
 
+PROJECT_ROOT = Path(__file__).parent.parent
+
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 400
 TILE_SIZE = 48
@@ -9,4 +11,4 @@ FPS = 60
 SCALE = 3
 WINDOW_SIZE = (WINDOW_WIDTH, WINDOW_HEIGHT)
 GAME_NAME = "Panacea"
-ASSETS = Path("Assets")
+ASSETS = PROJECT_ROOT / "assets"
